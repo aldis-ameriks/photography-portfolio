4cc31455a018a9f7bce629c180258368ccad1b01
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-05-07 09:17:28 +0300

    Remove unused component

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import Footer from './Footer'
 import Header from './Header'
 import Layout from './Layout'
 import ProjectHeader from './ProjectHeader'
-import ProjectPagination from './ProjectPagination'
 import SEO from './SEO'
 
-export { Card, Footer, Header, Layout, ProjectHeader, ProjectPagination, SEO }
+export { Card, Footer, Header, Layout, ProjectHeader, SEO }
