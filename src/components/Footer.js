6738ed6f205e2970b48b15fd02dcc7d1d127f2e1
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-05-08 13:25:24 +0300

    Update footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,19 @@ import styled from 'styled-components'
 const Content = styled.footer`
   color: ${props => props.theme.colors.primary};
   text-align: center;
-  font-size: 0.9rem;
+  font-size: 0.85rem;
   padding-top: 3rem;
   padding-bottom: 3rem;
   background: ${props => props.theme.colors.bg};
 `
 
-const Footer = () => <Content>Design and pictures by Aldis Ameriks &copy; 2019</Content>
+const Footer = () => (
+  <Content>
+    <a href="https://github.com/aldis-ameriks/photography-portfolio" rel="noreferrer">
+      code
+    </a>
+    , design and pictures by Aldis Ameriks &copy; 2019
+  </Content>
+)
 
 export default Footer
