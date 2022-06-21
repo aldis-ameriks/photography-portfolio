8416659abe399d69a2f252507fb4b2e57b88b4a4
Aldis Ameriks <aldis.ameriks@gmail.com>
2022-06-21 08:48:30 +0300

    Update dependencies

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -93,7 +93,7 @@ const Head = (props) => {
         '@type': 'Person',
         name: config.author,
       },
-      copyrightYear: postNode.parent.birthtime,
+      copyrightYear: postNode.parent.birthTime,
       creator: {
         '@type': 'Person',
         name: config.author,
@@ -106,7 +106,7 @@ const Head = (props) => {
           url: `${homeURL}${config.siteLogo}`,
         },
       },
-      datePublished: postNode.parent.birthtime,
+      datePublished: postNode.parent.birthTime,
       dateModified: postNode.parent.mtime,
       description,
       headline: title,
