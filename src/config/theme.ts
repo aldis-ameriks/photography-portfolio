87e60bc9056ef535b5cf2329f2d016396d7d94bc
Aldis Ameriks <aldis.ameriks@gmail.com>
2023-12-28 10:34:11 +0200

    Clean up theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,6 +1,3 @@
-// You can grab your own pattern here:
-// http://www.heropatterns.com/
-
 export default {
   bgPattern: ``,
   colors: {
@@ -10,18 +7,18 @@ export default {
     dark_bg: 'black',
     secondary: '#b6b6b6',
     link: 'black',
-    linkHover: '#79a8ff',
+    linkHover: '#79a8ff'
   },
   maxWidths: {
     general: '1600px',
-    project: 1600,
+    project: 1600
   },
   gridColumns: 2, // Number of columns of the grid on the index page
-  contentPadding: '1.0875rem',
+  contentPadding: '1rem',
   breakpoints: {
     xs: '400px',
     s: '600px',
     m: '900px',
-    l: '1200px',
-  },
+    l: '1200px'
+  }
 }
