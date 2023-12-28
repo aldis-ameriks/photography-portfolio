87998316c8362b7bfc742bb3cb29e40d72f5ded1
Aldis Ameriks <aldis.ameriks@gmail.com>
2023-12-28 11:13:16 +0200

    Update dependencies

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import '@testing-library/cypress/add-commands'
-import 'gatsby-cypress/commands'
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
